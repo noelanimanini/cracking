bdf988d8fdf2fccc847ece5968c2dfddf355e7ba
crystal <noelani.villanueva@gmail.com>
2021-08-06 18:08:08 -0700

    remove

---
+++ linkedlists/remove.js
@@ -0,0 +1,39 @@
+class Node {
+  constructor(val) {
+    this.val = val;
+    this.next = null;
+  }
+}
+
+class SinglyLinkedList {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+    this.length = 0;
+  }
+
+  push(val) {
+    let newNode = new Node(val);
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      this.tail.next = newNode;
+      this.tail = newNode;
+    }
+    this.length++;
+    return this;
+  }
+  remove(index) {
+    if (index < 0 || index > this.length) return undefined;
+    if (index === this.length - 1) return this.pop();
+    if (index === 0) return this.shift();
+
+    //create a variable for the previous node,
+    let preNode = this.get(index - 1);
+    let removed = preNode.next;
+    preNode.next = removed.next;
+    this.length--;
+    return removed;
+  }
+}
