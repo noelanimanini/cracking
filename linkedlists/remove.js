425f0a8d6a10d5fc0a40dad17df9773816e44ba3
crystal <noelani.villanueva@gmail.com>
2021-08-09 13:20:34 -0700

    remove

--- linkedlists/remove.js
+++ linkedlists/remove.js
@@ -25,15 +25,32 @@ class SinglyLinkedList {
     return this;
   }
   remove(index) {
-    if (index < 0 || index >= this.length) return undefined;
-    if (index === this.length) return this.pop();
+    if (index < 0 || index > this.length) return undefined;
     if (index === 0) return this.shift();
-
-    //get the previous node
-    let prev = this.get(index - 1);
+    if (index === this.length - 1) return this.pop();
+    let count = 0;
+    let current = this.head;
+    let prev = current;
+    while (count < index) {
+      count++;
+      prev = current;
+      current = current.next;
+    }
+    //save the next value
     let removed = prev.next;
+    //reassign the pointer
     prev.next = removed.next;
+    //decrement the length
     this.length--;
+    console.log("removed", removed);
     return removed;
   }
 }
+
+let list = new SinglyLinkedList();
+list.push(1);
+list.push(2);
+list.push(4);
+list.push(6);
+list.remove(2);
+console.log(list);
