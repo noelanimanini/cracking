f85cb90e33e34309a896dfb88c4cfc98276306d0
crystal <noelani.villanueva@gmail.com>
2021-08-11 12:27:34 -0700

    quick sort

--- sorting/quickSort.js
+++ sorting/quickSort.js
@@ -2,24 +2,17 @@ function quickSort(array) {
   //base case
   if (array.length <= 1) return array;
 
-  // let pivotIndex = Math.floor(array.length / 2);
-  //grab the first element in the array to be the pivot
+  //create the first element for the array, and two empty arrays
   let pivot = array.shift();
-  //initialize two empty arrays
   let less = [];
   let greater = [];
 
   //loop through the array
-  for (let i in array) {
-    //assign and initialize array values to a variable
-    const values = array[i];
-    //if the current value is less than or equal to the pivot, then push the less than or equal to value to the less array else, the greater array
-    values <= pivot ? less.push(values) : greater.push(values);
+  for (let ele of array) {
+    let value = array[ele];
+    value <= pivot ? less.push(value) : greater.push(value);
   }
-
-  console.log([...quickSort(less), pivot, ...quickSort(greater)]);
-  //return the concatenated recursive array using the spread operator.
-  return [...quickSort(less), pivot, ...quickSort(greater)];
+  return [...less, pivot, ...greater];
 }
 
 //Big O is o (n log n). This means that the algorithm is very efficient.
