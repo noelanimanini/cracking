1a01f9027a2d9a39855283eec39d391220a15887
crystal <noelani.villanueva@gmail.com>
2022-02-09 20:24:54 -0800

    two sum practice

---
+++ practice/practice.js
@@ -0,0 +1,49 @@
+/*
+Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+
+You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+You can return the answer in any order.
+
+ 
+
+Example 1:
+
+Input: nums = [2,7,11,15], target = 9
+Output: [0,1]
+Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+Example 2:
+
+Input: nums = [3,2,4], target = 6
+Output: [1,2]
+Example 3:
+
+Input: nums = [3,3], target = 6
+Output: [0,1]
+*/
+
+/*
+plan: 
+R: given an array and a target, return the indices from least to greatest. 
+P: loop through the array and apply logic of subtracting the target from each iteration.
+create a new array and push. 
+sort. 
+*/
+
+const practiceFunc = (array, target) => {
+  let newArr = [];
+  for (let i = 0; i < array.length; i++) {
+    let curr = array[i];
+    if (curr + array[i + 1] === target) {
+      newArr.push(i, i + 1);
+    } else {
+      newArr;
+    }
+  }
+  console.log(newArr);
+  return newArr;
+};
+
+// practiceFunc([2, 7, 11, 15], 9);
+//practiceFunc([3, 3], 6);
+complete! 
