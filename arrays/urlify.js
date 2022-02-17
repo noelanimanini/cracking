7de12129e1fab364afb3711d1b714399da4ca56b
crystal <noelani.villanueva@gmail.com>
2022-02-16 23:43:58 -0800

    urlify done!

--- arrays/urlify.js
+++ arrays/urlify.js
@@ -1,3 +1,19 @@
-const urlify = () => {};
+/*
+Write a method that replaces all the spaces in a string with %20
+*/
+const urlify = (string) => {
+  // let trim = string.trim();
+  // let result = trim.replaceAll(" ", "%20");
+  // console.log(result);
+  let cur = string.split(" ");
+  let result = [];
+  console.log(cur.length - 1);
+  for (let ele in cur) {
+    result.push(cur[ele]);
+    result.push("%20");
+  }
+  console.log(result.pop());
+  console.log(result.join(""));
+};
 
-urlify();
+urlify("Mr John Smith");
