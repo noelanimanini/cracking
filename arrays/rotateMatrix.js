215966588a3d0c5d72edf1e5e1df82d219459776
noelanimanini <crystalnvillanueva@gmail.com>
2022-02-21 22:28:22 -0800

    rotate the matrix

--- arrays/rotateMatrix.js
+++ arrays/rotateMatrix.js
@@ -1,31 +1,43 @@
-const rotateMatrix = (matrix) => {
-    let result = []
-    let zero = []
-    let one = []
-    let two = []
-    for (let ele of matrix) {
+// const rotateMatrix = (matrix) => {
+//     let result = []
+//     let zero = []
+//     let one = []
+//     let two = []
+//     for (let ele of matrix) {
         
-        if (ele[0]) {
-            zero.push(ele[0])
-            zero.reverse()
+//         if (ele[0]) {
+//             zero.push(ele[0])
+//             // zero.reverse()
            
-        } 
+//         } 
         
-         if (ele[1]) {
-            one.push(ele[1])
-            one.reverse()
-        } 
-         if (ele[2]) {
-            two.push(ele[2])
-            two.reverse()
-        }
-    }
-    result.push(zero, one , two)
-    console.log(result)
+//          if (ele[1]) {
+//             one.push(ele[1])
+//             // one.reverse()
+//         } 
+//          if (ele[2]) {
+//             two.push(ele[2])
+//             // two.reverse()
+//         }
+//     }
+//     result.push(zero.reverse(), one.reverse() , two.reverse())
+//     console.log(result)
 
+// }
+
+function rotate(matrix) {          // function statement
+    const N = matrix.length - 1;   // use a constant
+    // use arrow functions and nested map;
+    const result = matrix.map((row, i) => 
+         row.map((val, j) => matrix[N - j][i])
+    );
+    matrix.length = 0;       // hold original array reference
+    matrix.push(...result);  // Spread operator
+    console.log(matrix)
+    return matrix;
 }
-
-rotateMatrix([[1,2,3],[4,5,6],[7,8,9]])
+// rotate([[1,2,3],[4,5,6],[7,8,9]])
+rotate([[1,2],[3,4]])
 
 /*
 Question: Given a matrix, create a function that can rotate it 90 degrees. 
