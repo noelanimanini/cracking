2c445c42c1ab90bc7d1f0fd4d19e37e6906ccdaf
noelanimanini <crystalnvillanueva@gmail.com>
2022-02-22 21:30:16 -0800

    struggling with matrices

--- arrays/zeroMatrix.js
+++ arrays/zeroMatrix.js
@@ -1,5 +1,20 @@
-const zeroMatrix = () => {
-
+const zeroMatrix = (matrix) => {
+    const N = matrix.length - 1;
+    const result = matrix.map((row, i) => {
+        row.map((val, j) => {
+            if (val === 0) {
+                matrix[N - j][i] = 0 
+            }
+        })
+        // console.log(row)
+    })
+    return result
 }
 
-zeroMatrix()
+zeroMatrix([[1,2],[1,0,2]])
+
+/*
+Write an algorithm such that if an element in an M x N matrix is 0, it's entire row and column are set to 0
+*/ 
+
+// come back to this and work on some easier matrix practice problems
