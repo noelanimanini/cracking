9df558dd00dd775988fd658a2e5ba380d0d24638
noelanimanini <crystalnvillanueva@gmail.com>
2022-02-23 00:35:12 -0800

    factorialization practice

---
+++ practice/factorialize.js
@@ -0,0 +1,12 @@
+function FirstFactorial(num) { 
+    for (let i = num -1; i >= 1; i--) {
+      num = num * i
+    }
+    console.log(num)
+    return num
+   
+   }
+
+   FirstFactorial(8)
+
+   https://www.coderbyte.com/editor/First%20Factorial:JavaScript?utm_campaign=NewHomepage
