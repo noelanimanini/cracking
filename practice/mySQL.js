b5e51736df53de264c909e063cc1b7c02a559696
noelanimanini <crystalnvillanueva@gmail.com>
2022-03-06 00:57:19 -0800

    added the link

--- practice/mySQL.js
+++ practice/mySQL.js
@@ -1,1 +1,2 @@
 // SELECT Person.firstName, Person.lastName, Address.city, Address.state FROM Person LEFT JOIN Address ON Address.personId=Person.personId 
+// https://leetcode.com/problems/combine-two-tables/
