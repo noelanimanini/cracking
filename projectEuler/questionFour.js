4a293de6c33f866128fc7af54239d9d2561459ef
noelanimanini <crystalnvillanueva@gmail.com>
2022-03-07 19:53:35 -0800

    created the hash folder

--- projectEuler/questionFour.js
+++ projectEuler/questionFour.js
@@ -1,52 +1,18 @@
 const questionFour = () => {
-    let result; 
-    let i = 1 
     let first = 900
-    let second = 900 
-    // while ( i < 1000) {
-    //     let product = first * first+i
-        
-    //     if (product === Number(product.toString().split().reverse())) {
-           
-    //         result = product
-    //         break
-    //     }
-    //     i++
-    // }
-    
-    let j = 1
-    let k = 1 
-    // while (first < 1000 && second < 1000) {
-    //     let product = (first+j) * (second+k) 
-     
-    //     let num = Number(product.toString().split("").reverse().join(''))
-    //    if (product === num) {
-    //     result = product
-    //     break;
-    //    }
-    //     k++
-    //     j++
-    // }
-    // console.log(result)
-    // return result
+   let result; 
+    for (let i=1; i <= 99; i++) {
+        // console.log(first + i + j)
+        for (let j=1; j <= 99; j++) {
+           let product = (first + i ) * first
+              
+           if (product === Number(product.toString().split("").reverse().join(""))){
+            console.log("does this work")
+           }
+        }
 
-    while (first < 1000 && i <= 100) {
-        
-        while (second < 1000 && j <= 100) {
-            let product = (first+i)* (second+j)
-            // console.log("backwards "+Number(product.toString().split().reverse()))
-            // console.log("forwards "+product)
-            if (product === Number(product.toString().split("").reverse().join(''))) {
-                        result = product
-                        
-                    }
-            // console.log("j "+j )
-            // console.log("i " + i )
-            j++
-            i++
-                }
     }
-    console.log("result "+result)
+//  console.log(result)
 }
 
 questionFour()
