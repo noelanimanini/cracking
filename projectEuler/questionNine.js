baf951e15510f3b7453d2ae5147b6509ef615b8b
noelanimanini <crystalnvillanueva@gmail.com>
2022-03-11 21:39:05 -0800

    math

---
+++ projectEuler/questionNine.js
@@ -0,0 +1,14 @@
+const questionNine = (num) => {
+    let ans = 0
+    
+    for (let i=0; i <= num; i++) {
+        let c = i + 2
+        let a = i
+        while (c < num) {
+            if (a + )
+        }
+    }
+    console.log(ans)
+}
+
+questionNine(24)
