96112594c9429f96e58fde3f91d0a7c7b8b7a2d0
noelanimanini <crystalnvillanueva@gmail.com>
2022-03-12 23:49:15 -0800

    redo this problem in the morning, you are fried and cannot read

--- practice/highestProfit.js
+++ practice/highestProfit.js
@@ -1,19 +1,43 @@
-const highestProfit = (nums) => {
-    let hash = new Map
-
-    for (let i=1; i <= nums.length; i++) {
-        let highest = 0
-        let lowest = 0
-        if (nums[i] > highest) {
-            highest = nums[i]
-            hash.set(highest, i)
-        }
-        if (nums[i] < highest) {
-            lowest = nums[i]
-            hash.set(lowest, i)
+const highestProfit = (prices) => {
+    if (prices.length === 1) return 0
+    if (prices.length === 2 && prices[0] < prices[1]) console.log( prices[1] - prices[0] )
+    if (prices.length === 2 && prices[0] > prices[1]) console.log( prices[0] - prices[1] )
+    
+    
+    let highest = 0
+    let lowest = 0
+    let index = 0
+    let index2 = 0
+    for (let i=1; i < prices.length; i++) {
+        let cur = prices[i]
+        if (cur > highest) {
+            highest = cur
+            index = i
+         
+        }  
+    }
+   
+    for (let i=1; i < prices.length; i++) {
+        let cur2 = prices[i]
+        if (cur2 < prices[i+1]) {
+            lowest = cur2
+            index2 = i
+            break;
+        } 
+        else {
+            index2 = i
+            
         }
     }
-    console.log(hash)
+   
+    if (index < index2) {
+        console.log(0)
+        return 0
+    } else if (index > index2){
+        // console.log(lowest)
+        console.log(highest - lowest)
+        return highest - lowest
+    }
 }
 
-highestProfit( [7,1,5,3,6,4])
+highestProfit([1,2])
