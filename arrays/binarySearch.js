30b5c6bb9077f177c45405aacdb7c2582fa17a03
noelanimanini <crystalnvillanueva@gmail.com>
2022-03-16 21:36:41 -0700

    this was truly the worst experience of my list

---
+++ arrays/binarySearch.js
@@ -0,0 +1,25 @@
+
+function recursiveSearch(list, item) {   
+    let low = 0
+    let high = list.length - 1
+  
+    while (low <= high) {
+      const mid = Math.floor((low + high) / 2)
+      const guess = list[mid]
+  
+      if (guess === item) {
+        console.log(mid)
+        return mid
+      }
+  
+      if (guess > item) {
+        high = mid - 1
+      } else {
+        low = mid + 1
+      }
+    }
+  
+    return null 
+}
+
+recursiveSearch([-1,0,3,5,9,12], 9)
