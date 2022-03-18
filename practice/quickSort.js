b3973f321c00c1746d8060158b4471bc9d6cfb0e
noelanimanini <crystalnvillanueva@gmail.com>
2022-03-17 21:27:45 -0700

    quick sort arjuns version

--- practice/quickSort.js
+++ practice/quickSort.js
@@ -1,15 +1,23 @@
 const quickSort = (array) => {
-    if (array.length <= 1) return array 
+    if (array.length < 2) {
+        return array 
+    } else {
     let left = []
     let right = []
 
-    let pivot = array[0]
+    let pivot = array[Math.floor(Math.random()* (array.length-1))]
 
     for (let i=1; i < array.length; i++) {
-        array[i] < pivot ? left.push(array[i]) : right.push(array[i])
+        if (array[i] <= pivot) {
+            left.push(array[i])
+        }
+        if (array[i] > pivot) {
+            right.push(array[i])
+        }
     }
     
     console.log(quickSort(left).concat(pivot, quickSort(right)))
     return quickSort(left).concat(pivot, quickSort(right))
+    }
 }
 quickSort([14,2,3,6])
