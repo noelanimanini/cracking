3a3db8364818a191912af01bc703d184429c8ab6
noelanimanini <crystalnvillanueva@gmail.com>
2022-03-22 20:48:17 -0700

    linked lists

---
+++ linkedlists/leetCode876.js
@@ -0,0 +1,20 @@
+var middleNode = function(head) {
+    //     let A = [head];
+    //     while (A[A.length - 1].next != null) {
+    //          A.push(A[A.length - 1].next);
+    //         console.log( A.push(A[A.length - 1].next))
+    //     }
+           
+    //     return A[Math.trunc(A.length / 2)];
+       let fast = head
+       let slow = head 
+       
+       while (fast && fast.next) {
+           slow = slow.next
+        
+           fast = fast.next.next 
+           
+       }
+        return slow 
+    };
+    
