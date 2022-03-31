79a999ffc70dfdebaee01281c1cd5688de0d5ebb
noelanimanini <crystalnvillanueva@gmail.com>
2022-03-30 22:10:50 -0700

    SQL practice

---
+++ SQL/leetCode1757.js
@@ -0,0 +1,8 @@
+// # Write your MySQL query statement below
+
+
+// # given the table Products, return a query the finds all the IDs where the product is both low in fat and is recyclable
+
+// # select all ids where low_fats="Y" AND recycleable="Y"
+
+// SELECT product_id FROM Products WHERE low_fats="Y" AND recyclable="Y"
