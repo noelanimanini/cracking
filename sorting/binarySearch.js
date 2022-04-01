22b2e74bd3cdb150e81c0bec76811019b554bc03
noelanimanini <crystalnvillanueva@gmail.com>
2022-03-31 18:45:34 -0700

    binary search

--- sorting/binarySearch.js
+++ sorting/binarySearch.js
@@ -1,11 +1,15 @@
 var search = function(nums, target) {
+    //create the min 
     let min = 0 
+    //create the max 
     let max = nums.length-1
-    
+    //while the min is less than or equal to the max
     while (min <= max) {
+        //find the middle 
         let guess = Math.floor((min+max)/2)
-        
+        //if the numbers are equal to the target
         if (nums[guess] === target) {
+            //return the index
             return guess 
         } else if (nums[guess] < target) {
             min = guess + 1
@@ -13,5 +17,6 @@ var search = function(nums, target) {
             max = guess - 1
         }
     }
+    //else return -1
     return -1
 };
