e826246dd5b945a41a00dfa092646553007c032a
noelanimanini <crystalnvillanueva@gmail.com>
2022-04-02 09:53:18 -0700

    group by and sum()

---
+++ SQL/leetcode1741.js
@@ -0,0 +1,28 @@
+// select event_day as "day", emp_id, SUM(out_time - in_time) as "total_time" from Employees GROUP BY day, emp_id;
+
+/*
+
+this problem https://leetcode.com/problems/find-total-time-spent-by-each-employee/ 
+
+Employees table:
++--------+------------+---------+----------+
+| emp_id | event_day  | in_time | out_time |
++--------+------------+---------+----------+
+| 1      | 2020-11-28 | 4       | 32       |
+| 1      | 2020-11-28 | 55      | 200      |
+| 1      | 2020-12-03 | 1       | 42       |
+| 2      | 2020-11-28 | 3       | 33       |
+| 2      | 2020-12-09 | 47      | 74       |
++--------+------------+---------+----------+
+Output: 
++------------+--------+------------+
+| day        | emp_id | total_time |
++------------+--------+------------+
+| 2020-11-28 | 1      | 173        |
+| 2020-11-28 | 2      | 30         |
+| 2020-12-03 | 1      | 41         |
+| 2020-12-09 | 2      | 27         |
++------------+--------+------------+
+
+the GROUP BY clause is often used with the sum() clause
+*/ 
