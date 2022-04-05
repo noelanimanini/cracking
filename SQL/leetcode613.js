4fbc9f23508dd8b55b6dfac7d892cdfdd1caa068
noelanimanini <crystalnvillanueva@gmail.com>
2022-04-04 19:25:39 -0700

    inner join practice

---
+++ SQL/leetcode613.js
@@ -0,0 +1,11 @@
+
+// select 
+// finding the minimum and using the absolute value 
+// min(abs(p1.x - p2.x)) 
+// as shortest
+// from 
+// inner join 
+// point p1
+// join 
+// point p2
+// on p1.x != p2.x
