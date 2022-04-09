84a1908e710d69a92e64a42e1a8ee77bb361c279
noelanimanini <crystalnvillanueva@gmail.com>
2022-04-08 22:56:19 -0700

    removing the max

--- trees/removemaxheap.js
+++ trees/removemaxheap.js
@@ -31,7 +31,55 @@ class MaxHeap {
         }
     }
 
-    
+    extractMax() {
+        const max = this.values[0]
+        const end = this.values.pop()
+
+        this.values[0] = end 
+        //trickle down 
+        this.sinkDown()
+        // console.log(max)
+        return max 
+    }
+
+    sinkDown(){
+        let index = 0
+        const length = this.values.length 
+        const element = this.values[0]
+
+        while (true) {
+            let leftChildIndex = 2 * index + 1
+            let rightChildIndex = 2 * index + 2
+
+            let leftchild, rightchild; //checking for out of bounds
+
+            let swap = null; 
+
+            if (leftChildIndex < length) {
+                leftchild = this.values[leftChildIndex]
+                if (leftchild > element) {
+                    swap = leftChildIndex
+                }
+            }
+
+            if (rightChildIndex < length) {
+                rightchild = this.values[rightChildIndex]
+                if (swap = null && rightchild > element || (swap !== null && rightchild > leftchild)) {
+                    swap = rightChildIndex
+                    index = swap
+                }
+            }
+
+            if (swap === null) break; 
+
+            this.values[index] = this.values[swap]
+            this.values[swap] = element
+ 
+
+
+        }
+    }
+
 }
 
 let heap = new MaxHeap()
@@ -42,4 +90,4 @@ heap.insert(18);
 heap.insert(27);
 heap.insert(12);
 heap.insert(55);
-console.log(heap)
+console.log(heap.extractMax())
