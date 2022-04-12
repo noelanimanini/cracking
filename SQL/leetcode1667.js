4298ac816196cc71d1bdc309731af9e764c53969
noelanimanini <crystalnvillanueva@gmail.com>
2022-04-11 20:16:11 -0700

    sql review

---
+++ SQL/leetcode1667.js
@@ -0,0 +1,8 @@
+// Select 
+//     Users.user_id,
+//     concat(
+//         upper( substr( Users.name,1,1 ) ),
+//         lower( substr( Users.name, 2 ) )
+//         ) as name
+// from Users
+// order by Users.user_id
