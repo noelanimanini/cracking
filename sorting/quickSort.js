08b3d75541419d66f98118bdb72103b9437004fc
noelanimanini <crystalnvillanueva@gmail.com>
2022-04-12 13:45:04 -0700

    a different way to do quicksort

--- sorting/quickSort.js
+++ sorting/quickSort.js
@@ -3,14 +3,14 @@ function quickSort(values) {
 
  let left = []
  let right = []
- let pivot = values.pop()
+ let pivot = values[0]
 
- for (let i=0; i < values.length; i++) {
+ for (let i=1; i < values.length; i++) {
    values[i] <= pivot ? left.push(values[i]) : right.push(values[i])
  }
 
-
- return [...quickSort(left), pivot, ...quickSort(right)]
+ console.log(quickSort(left).concat(pivot, quickSort(right)))
+ return quickSort(left).concat(pivot, quickSort(right))
 }
 
 //Big O is o (n log n). This means that the algorithm is very efficient.
